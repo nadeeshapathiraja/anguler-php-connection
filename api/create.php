61052ddc928beb7bfb247c5dd470c79662c03ff3
nadeeshapathiraja <pdncpathiraja95@gmail.com>
2019-05-06 00:51:15 +0530

    try to complete connectin-create

--- api/create.php
+++ api/create.php
@@ -17,20 +17,48 @@ if(isset($postdata) && !empty($postdata))
   }
 
   // Sanitize.
-  $number = mysqli_real_escape_string($con, trim($request->number));
-  $amount = mysqli_real_escape_string($con, (int)$request->amount);
+  
+  $indexnumber = mysqli_real_escape_string($con, (int)$request->indexnumber);
+  $fullname = mysqli_real_escape_string($con, trim($request->fullname));
+  $ininame = mysqli_real_escape_string($con, trim($request->ininame));
+  $address = mysqli_real_escape_string($con, trim($request->address));
+  $dob = mysqli_real_escape_string($con, trim($request->dob));
+  $gender = mysqli_real_escape_string($con, trim($request->gender));
+  $school = mysqli_real_escape_string($con, trim($request->school));
+  $grade = mysqli_real_escape_string($con, trim($request->grade));
+  $parentname = mysqli_real_escape_string($con, trim($request->parentname));
+  $nic = mysqli_real_escape_string($con, trim($request->nic));
+  $mobile = mysqli_real_escape_string($con, (int)$request->mobile);
+  $fixed = mysqli_real_escape_string($con, (int)$request->fixed);
+  $email = mysqli_real_escape_string($con, trim($request->email));
+  
 
 
   // Create.
-  $sql = "INSERT INTO `policies`(`id`,`number`,`amount`) VALUES (null,'{$number}','{$amount}')";
+  $sql = "INSERT INTO `policies`(`userid`,`indexnumber`,`fullname`,`ininame`,`address`,`dob`,`gender`,`school`,`grade`,`parentname`,`nic`,`mobile`,`fixed`,`email`) VALUES 
+  (null,'{$indexnumber}','{$fullname}','{$ininame}','{$address}','{$dob}','{$gender}','{$school}','{$grade}','{$parentname}','{$nic}'
+  ,'{$mobile}','{$fixed}','{$email}')";
 
   if(mysqli_query($con,$sql))
   {
     http_response_code(201);
     $policy = [
-      'number' => $number,
-      'amount' => $amount,
-      'id'    => mysqli_insert_id($con)
+      'indexnumber' => $indexnumber,
+      'fullname' => $fullname,
+      'ininame' => $ininame,
+      'address' => $address,
+      'dob' => $dob,
+      'gender' => $gender,
+      'school' => $school,
+      'grade' => $grade,
+      'parentname' => $parentname,
+      'nic' => $nic,
+      'mobile' => $mobile,
+      'fixed' => $fixed,
+      'email' => $email,
+
+
+      'userid'    => mysqli_insert_id($con)
     ];
     echo json_encode($policy);
   }
